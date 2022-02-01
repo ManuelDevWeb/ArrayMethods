eb4a17f1725c41cb56a3cba0e121f8289ee79fc3
Manuel V <manueldeveloper17@gmail.com>
2022-02-01 00:20:30 -0500

    Se finalizó con el método Every

--- dataSheet.js
+++ dataSheet.js
@@ -134,9 +134,28 @@ const byGender = characters.sort((a, b) => {
 
 //***EVERY***
 //1. Does every character have blue eyes?
+const allBlueEyes = characters.every(character => (
+    character.eye_color === 'blue'
+));
+// console.log(allBlueEyes);
+
 //2. Does every character have mass more than 40?
+const allMassMoreThan40 = characters.every(character => (
+    character.mass > 40
+));
+// console.log(allMassMoreThan40);
+
 //3. Is every character shorter than 200?
+const allShorterThan200 = characters.every(character => (
+    character.height < 200
+));
+// console.log(allShorterThan200);
+
 //4. Is every character male?
+const allMale = characters.every(character => (
+    character.gender === 'male'
+));
+// console.log(allMale);
 
 //***SOME***
 //1. Is there at least one male character?
